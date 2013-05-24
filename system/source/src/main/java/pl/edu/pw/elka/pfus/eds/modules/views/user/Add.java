3ecb6a686db443df29491f70f90c92e6d454daf3
f00sek <biale.jerozy@gazeta.pl>
2013-05-24 19:37:44 +0200

    Lista uzytkownikow dziala, blad 404 obsluzony

---
+++ system/source/src/main/java/pl/edu/pw/elka/pfus/eds/modules/views/user/Add.java
@@ -0,0 +1,4 @@
+package pl.edu.pw.elka.pfus.eds.modules.views.user;
+
+public class Add {
+}
---
+++ system/source/src/main/java/pl/edu/pw/elka/pfus/eds/modules/views/user/Index.java
@@ -0,0 +1,32 @@
+package pl.edu.pw.elka.pfus.eds.modules.views.user;
+
+import org.apache.log4j.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.Template;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.mvc.builders.AbstractBuilder;
+import org.objectledge.web.mvc.builders.BuildException;
+import pl.edu.pw.elka.pfus.eds.domain.dao.UserDao;
+import pl.edu.pw.elka.pfus.eds.domain.entity.User;
+
+import java.util.List;
+
+public class Index extends AbstractBuilder {
+    private static final Logger logger = Logger.getLogger(Index.class);
+    private UserDao userDao;
+
+    public Index(Context context, UserDao userDao) {
+        super(context);
+        this.userDao = userDao;
+    }
+
+    @Override
+    public String build(Template template, String embeddedBuildResults) throws BuildException, ProcessingException {
+        TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
+        List<User> users = userDao.getAllUsers();
+        logger.info("Obtained: " + users);
+        templatingContext.put("users", users);
+        return super.build(template, embeddedBuildResults);
+    }
+}
